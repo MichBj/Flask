b9411680b6c2225620fdaa3bc56ef1749593a84a
ll M ll <josmike97@gail.com>
2025-08-18 01:00:21 -0500

    semana 9

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 # Define una ruta para la URL principal ('/')
 @app.route('/')
 def inicio():
-    """
-    Maneja las solicitudes a la URL principal y devuelve un mensaje de bienvenida.
-    """
+    """ Maneja las solicitudes a la URL principal y devuelve un mensaje de bienvenida. """
     return '¡Hola! Esta es mi aplicación Flask.'
+
+# Define la ruta para un usuario con un nombre personalizado
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'¡Bienvenido, {nombre}!'
 
 # Este bloque asegura que la aplicación se ejecute solo si el script se ejecuta directamente
 if __name__ == '__main__':
-    # Inicia el servidor de desarrollo
-    # debug=True activa el depurador y el auto-recargador
+    # Inicia el servidor de desarrollo en modo de depuración
     app.run(debug=True)
